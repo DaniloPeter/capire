cb4cf544bc0567a3b57d08537c60da2dbca682d6
DaniloPeter <gomerplayer@gmail.com>
2025-01-14 18:38:54 +0300

    oData v2

--- app/serverside-rendering/webapp/controller/Main.controller.js
+++ app/serverside-rendering/webapp/controller/Main.controller.js
@@ -1,5 +1,5 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/model/odata/v4/ODataModel"],
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/odata/v2/ODataModel"],
   (Controller, ODataModel) => {
     "use strict";
     return Controller.extend("be.wl.serversiderendering.controller.Home", {
@@ -11,9 +11,10 @@ sap.ui.define(
           json: true,
           useBatch: false,
         });
+        debugger;
         this.getView().setModel(this._oModel);
-
         this._oSmartFilterBar = this.byId("smartFilterBar");
+        debugger;
       },
     });
   }
